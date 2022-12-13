7c02634b32d3495851878ae249a563bd4bed2e08
younseochoi <younseo42@gmail.com>
2022-12-13 16:14:15 +0900

    feat: jsp 연결
    추가한 것
    1. src/main/resources
    2. src/main/webapp/WEB-INF/views
    3. src/main/java test패키지 (테스트용)

--- src/main/java/toyproject/member/ToyprojectApplication.java
+++ src/main/java/toyproject/member/ToyprojectApplication.java
@@ -2,7 +2,9 @@ package toyproject.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "test")
 @SpringBootApplication
 public class ToyprojectApplication {
 
